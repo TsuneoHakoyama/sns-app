c4e1ce66881cf41864469a29fa66368fa69017d9
Tsuneo Hakoyama <tunehako@gmail.com>
2025-06-18 15:30:17 +0900

    feat(app): create Home, Signin and Signup components and set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import { Home } from "./pages/Home";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+
 export function App() {
   return (
     <>
-      <div>
-        Hello World!
-      </div>
+      <BrowserRouter>
+        <div>
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,5 @@
+export function Signin() {
+    return (
+        <h1>Sign-in画面</h1>
+    )
+}
