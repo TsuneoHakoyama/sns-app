e5f6ddd1f90241e4ca6d750221d202fe3e5bde4c
Tsuneo Hakoyama <tunehako@gmail.com>
2025-06-20 00:46:13 +0900

    feat(auth): add Context Provider for global login state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import { App } from './App.jsx'
+import { SessionProvider } from './SessionProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SessionProvider>
+      <App />
+    </SessionProvider>
   </StrictMode>,
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,13 @@
+import { useContext } from "react"
+import { Navigate } from "react-router";
+
+import { SessionContext } from "../SessionProvider"
+
 export function Home() {
+    const { currentUser } = useContext(SessionContext);
+
+    if (currentUser == null) return <Navigate replace to="/signin" />;
+
     return (
         <h1>Home画面</h1>
     )
